753ece344985edfa437e964f765c59968b326207
José Antonio Escudero García <jaeg90@gmail.com>
2023-05-09 19:30:34 +0200

    Button and routes done

--- routes/responses.routes.js
+++ routes/responses.routes.js
@@ -2,19 +2,23 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 
 const Response = require("./../models/Response.model");
-
 const { isLoggedOut, isLoggedIn } = require("../middleware/route-guard.js");
 
 //GET /responses
 router.get("/", isLoggedIn, (req, res, next) => {
   Response.find()
     .then((allResponses) => {
+      // const responses = allResponses.map((el) => {
+      //   console.log(el.updatedAt.toLocaleDateString("es-ES"));
+      //   el.updatedAt = el.updatedAt.toLocaleDateString("es-ES");
+      //   console.log(el.newUpdatedAt);
+      //   return el;
+      // });
+      // console.log(responses);
       res.render("responses/responses.hbs", { responses: allResponses });
-      console.log(allResponses);
     })
     .catch((error) => {
       console.log("Error while getting the books from the DB: ", error);
-
       next(error);
     });
 });
@@ -24,21 +28,17 @@ router.get("/:responseId", isLoggedIn, (req, res) => {
   Response.findById(responseId)
     .then((response) => {
       res.render("responses/response.hbs", { response });
-      console.log({ responseId });
-      console.log(req.params);
     })
     .catch((err) => console.log(err));
 });
 
-router.get("/my-responses", isLoggedIn, (req, res) => {
+router.get("/:responseId/delete", isLoggedIn, (req, res, next) => {
   const { responseId } = req.params;
-  Response.findById(responseId)
-    .then((response) => {
-      res.render("responses/response.hbs", { response });
-      console.log({ responseId });
-      console.log(req.params);
+  Response.findByIdAndDelete(responseId)
+    .then(() => {
+      res.redirect("/responses");
     })
-    .catch((err) => console.log(err));
+    .catch((error) => next(error));
 });
 
 module.exports = router;

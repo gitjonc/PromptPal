ad0a24d3466f73f2413da72e85abb34cdd651d6c
José Antonio Escudero García <jaeg90@gmail.com>
2023-05-09 21:15:03 +0200

    Filters done

--- routes/responses.routes.js
+++ routes/responses.routes.js
@@ -5,22 +5,44 @@ const Response = require("./../models/Response.model");
 const { isLoggedOut, isLoggedIn } = require("../middleware/route-guard.js");
 
 //GET /responses
-router.get("/", isLoggedIn, (req, res, next) => {
-  Response.find()
-    .then((allResponses) => {
-      // const responses = allResponses.map((el) => {
-      //   console.log(el.updatedAt.toLocaleDateString("es-ES"));
-      //   el.updatedAt = el.updatedAt.toLocaleDateString("es-ES");
-      //   console.log(el.newUpdatedAt);
-      //   return el;
-      // });
-      // console.log(responses);
-      res.render("responses/responses.hbs", { responses: allResponses });
-    })
-    .catch((error) => {
-      console.log("Error while getting the books from the DB: ", error);
-      next(error);
-    });
+router.get("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const tags = await Response.distinct("tag");
+    const responses = await Response.find();
+    res.render("responses/responses", { tags, responses });
+  } catch (error) {
+    next(error);
+  }
+
+  // Response.distinct("tag").then((agg) => {
+  //   console.log(agg);
+  // });
+  // Response.find()
+  //   .then((allResponses) => {
+  //     // const responses = allResponses.map((el) => {
+  //     //   console.log(el.updatedAt.toLocaleDateString("es-ES"));
+  //     //   el.updatedAt = el.updatedAt.toLocaleDateString("es-ES");
+  //     //   console.log(el.newUpdatedAt);
+  //     //   return el;
+  //     // });
+  //     // console.log(responses);
+  //     res.render("responses/responses.hbs", { responses: allResponses });
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error while getting the books from the DB: ", error);
+  //     next(error);
+  //   });
+});
+
+router.post("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const tags = await Response.distinct("tag");
+    const tag = req.body.tag;
+    const responses = await Response.find({ tag: tag });
+    res.render("responses/responses", { tags, responses });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:responseId", isLoggedIn, (req, res) => {

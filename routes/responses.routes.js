0bb57b3c11ccc38b464570616c785859072350c6
José Antonio Escudero García <jaeg90@gmail.com>
2023-05-10 19:17:55 +0200

    Format date done

--- routes/responses.routes.js
+++ routes/responses.routes.js
@@ -9,29 +9,16 @@ router.get("/", isLoggedIn, async (req, res, next) => {
   try {
     const tags = await Response.distinct("tag");
     const responses = await Response.find();
-    res.render("responses/responses", { tags, responses });
+    const responsesUpdated = responses.map((el) => {
+      const element = el.toObject();
+      element.updatedAt = element.updatedAt.toLocaleDateString("es-ES");
+      return element;
+    });
+    console.log(responsesUpdated[0]);
+    res.render("responses/responses", { tags, responses: responsesUpdated });
   } catch (error) {
     next(error);
   }
-
-  // Response.distinct("tag").then((agg) => {
-  //   console.log(agg);
-  // });
-  // Response.find()
-  //   .then((allResponses) => {
-  //     // const responses = allResponses.map((el) => {
-  //     //   console.log(el.updatedAt.toLocaleDateString("es-ES"));
-  //     //   el.updatedAt = el.updatedAt.toLocaleDateString("es-ES");
-  //     //   console.log(el.newUpdatedAt);
-  //     //   return el;
-  //     // });
-  //     // console.log(responses);
-  //     res.render("responses/responses.hbs", { responses: allResponses });
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error while getting the books from the DB: ", error);
-  //     next(error);
-  //   });
 });
 
 router.post("/", isLoggedIn, async (req, res, next) => {

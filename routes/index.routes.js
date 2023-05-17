8c31d2a9d4ea4639e3c55213b40ae3ef7a5ff338
Jon <jon_ander_23@protonmail.com>
2023-05-17 16:14:55 +0200

    Ya coge la imagen del user el nav, prompts ajustados al diseño, se lanza pop-up

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -22,59 +22,65 @@ router.get("/sign-up", isLoggedOut, (req, res) => {
 });
 
 // POST /sign-up
-router.post("/sign-up", uploader.single("profilePic"), isLoggedOut, async (req, res, next) => {
-  try {
-    const { username, email, password, industry } = req.body;
-    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if (!regex.test(password)) {
-      res.status(500).render("auth/signup", {
-        errorMessage:
-          "El password debe tener al menos 6 caracteres y debe contener un número, una minúscula y una mayúscula.",
-      });
-      return;
-    }
+router.post(
+  "/sign-up",
+  uploader.single("profilePic"),
+  isLoggedOut,
+  async (req, res, next) => {
+    try {
+      const { username, email, password, industry } = req.body;
+      const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+      if (!regex.test(password)) {
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "El password debe tener al menos 6 caracteres y debe contener un número, una minúscula y una mayúscula.",
+        });
+        return;
+      }
 
-    if (!username || !email || !password) {
-      res.render("auth/signup", {
-        errorMessage: "Es necesario rellenar todos los campos.",
-      });
-      return;
-    }
+      if (!username || !email || !password) {
+        res.render("auth/signup", {
+          errorMessage: "Es necesario rellenar todos los campos.",
+        });
+        return;
+      }
 
-    const user = await User.findOne({ $or: [{ username }, { email }] });
-    if (user) {
-      res.render("auth/signup", {
-        errorMessage: "El usuario y/o email ya están en uso",
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+      if (user) {
+        res.render("auth/signup", {
+          errorMessage: "El usuario y/o email ya están en uso",
+        });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      await User.create({
+        username,
+        email,
+        password: hashedPassword,
+        industry,
+        profilePic: req.file.path,
       });
-      return;
-    }
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-    await User.create({
-      username,
-      email,
-      password: hashedPassword,
-      industry,
-      profilePic: req.file.path,
-    });
-    await sendMail({
-      to: email,
-      username: username,
-    });
+      await sendMail({
+        to: email,
+        username: username,
+      });
 
-    res.redirect("/profile");
-  } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError) {
-      res.status(500).render("auth/signup", { errorMessage: error.message });
-    } else if (error.code === 11000) {
-      res.status(500).render("auth/signup", {
-        errorMessage: "El usuario y el email deben ser únicos, y alguno está en uso.",
-      });
-    } else {
-      next(error);
+      res.redirect("/profile");
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "El usuario y el email deben ser únicos, y alguno está en uso.",
+        });
+      } else {
+        next(error);
+      }
     }
   }
-});
+);
 
 // GET /log-in
 router.get("/log-in", isLoggedOut, (req, res) => {
@@ -112,38 +118,44 @@ router.get("/profile", isLoggedIn, (req, res) => {
 
 router.get("/edit-profile", isLoggedIn, (req, res, next) => {
   const user = req.session.currentUser;
+  console.log(user);
   User.findById(user._id).then((userOne) => {
     res.render("auth/profile-account", userOne);
   });
 });
 
-router.post("/edit-profile", uploader.single("profilePic"), isLoggedIn, async (req, res, next) => {
-  //const user = req.session.currentUser;
-  console.log("funca");
-  const { username, email, industry, password } = req.body;
-  const user = await User.findOne({ email });
-  console.log(user);
-  console.log(password);
-  let pic = req.body.profilePicOld;
-  if (req.file != undefined) {
-    pic = req.file.path;
-  }
+router.post(
+  "/edit-profile",
+  uploader.single("profilePic"),
+  isLoggedIn,
+  async (req, res, next) => {
+    //const user = req.session.currentUser;
+    console.log("funca");
+    const { username, email, industry, password } = req.body;
+    const user = await User.findOne({ email });
+    console.log(user);
+    console.log(password);
+    let pic = req.body.profilePicOld;
+    if (req.file != undefined) {
+      pic = req.file.path;
+    }
 
-  if (!bcrypt.compareSync(password, user.password)) {
-    console.log("Funca 2");
-    const salt = bcrypt.genSaltSync(saltRounds);
-    password = bcrypt.hashSync(password, salt);
+    if (!bcrypt.compareSync(password, user.password)) {
+      console.log("Funca 2");
+      const salt = bcrypt.genSaltSync(saltRounds);
+      password = bcrypt.hashSync(password, salt);
+    }
+    console.log(password);
+    await User.findByIdAndUpdate(user._id, {
+      username,
+      email,
+      //password: password,
+      industry,
+      profilePic: pic,
+    });
+    res.redirect("/edit-profile");
   }
-  console.log(password);
-  await User.findByIdAndUpdate(user._id, {
-    username,
-    email,
-    //password: password,
-    industry,
-    profilePic: pic,
-  });
-  res.redirect("/edit-profile");
-});
+);
 
 // GET /log-out
 router.get("/log-out", isLoggedIn, (req, res) => {
